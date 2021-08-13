b194c14b76b21f2db4e2877fc667aadf39796cb1
vibhor-voicebot <53684148+vibhor-voicebot@users.noreply.github.com>
2021-08-13 22:57:39 +0000

    Update deploy_ansible.sh

--- deploy_ansible.sh
+++ deploy_ansible.sh
@@ -3,13 +3,13 @@
 #mkdir -p $HOME/var/tf
 #cd $HOME/var/tf
 
-git clone https://github.com/vibhor-voicebot/devopslabs.git
-cd devopslabs
+#git clone https://github.com/vibhor-voicebot/devopslabs.git
+#cd devopslabs
 
 #i=`cat version`
 echo "Building a docker image Apache 0.2"
 docker build -t apache:0.2 .
-
+wait
 ansible-playbook playbook.yml -e 'ansible_python_interpreter=/usr/bin/python3'
 
 docker ps
